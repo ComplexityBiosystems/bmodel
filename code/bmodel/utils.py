b4d98b8e50d3ef5e9dcada587303e5617640000a
Francesc Font Clos <francesc.font@gmail.com>
2018-12-09 22:55:35 +0100

    up

--- code/bmodel/utils.py
+++ code/bmodel/utils.py
@@ -4,7 +4,33 @@ Utility functions for boolean models.
 Francesc Font-Clos
 Oct 2018
 """
+from typing import Sequence, Any
 import numpy as np
+import pandas as pd
+
+
+def state_as_string(state: pd.Series, order: Sequence[Any] = None):
+    """Represent a boolean state as a string.
+
+    Transforms -1/1 int representation to 0/1 string representation.
+
+
+    Parameters
+    ----------
+    state : pd.Series
+        Boolean state in -1,1 representation
+    order : Sequence[Any], optional
+        How to order the variables. Will take series index if not passed.
+
+    Returns
+    -------
+    str
+        A string of the form '01110010' representing the boolean state.
+    """
+
+    if order is None:
+        order = state.index
+    return "".join(((state+1)/2).astype(int).astype(str).loc[order].values)
 
 
 def check_interaction_matrix(J):
